9dc0d0c4ecea967048c606ee96e2a59006c3cfda
siva <sivakrishnaboya45@gmail.com>
2023-08-31 12:53:47 +0530

    engine set

--- data/database.js
+++ data/database.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
 export const connectDB = ()=>{
-    mongoose.connect(process.env.MONGO_URI,{dbName:"backend"}).then(()=>{
-    console.log("Database is connected");
+    mongoose.connect(process.env.MONGO_URI,{dbName:"backend"}).then((c)=>{
+    console.log(`Database is connected with $${c.connection.host}`);
 }).catch((err)=>{
     console.log(err);
 })
